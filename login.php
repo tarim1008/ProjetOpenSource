ee9a9187d9df9006b9f1c6455f172f623c5b5a32
vagrant <vagrant@localhost.localdomain>
2021-05-07 04:16:35 +0000

    Modification de l'extension de html en php

---
+++ login.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Page html</title>
+</head>
+	<body>	
+		<h1>Bonjour la famille</h1>
+		<form method="post" action="login.php">
+			<label for="login">Login : </label><input type="text" name="login"/>
+			<label for="mdp">Mot de Passe : </label><input type="password" name="mdp"/>
+			<input type="submit" value="Se connecter" name="connecter" />
+			<input type="reset"  value="Effacer" name="effacer" />
+		</form>
+	</body>
+</html>
